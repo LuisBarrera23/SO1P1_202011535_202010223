ae1704debadf3d33bdc1ec4fafa0fe327f2f1a2e
Ennox2 <82472966+Ennox2@users.noreply.github.com>
2023-04-16 12:07:49 -0600

    gRPC

--- proyecto/API/server.js
+++ proyecto/API/server.js
@@ -1,17 +1,41 @@
 const express = require('express');
 const WebSocket = require('ws');
+const mysql = require('mysql');
+const cors = require('cors');
 
 // Crear una instancia de express y un servidor HTTP
 const app = express();
+app.use(cors());
 const server = require('http').createServer(app);
 
 // Crear una instancia de WebSocket y conectarla al servidor HTTP
 const wss = new WebSocket.Server({ server });
 
+const connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'root',
+  password : '1234',
+  database : 'db_so1'
+});
+
+connection.connect((err) => {
+  if (err) {
+    console.error('Error al conectarse a la base de datos: ', err);
+    process.exit(1);
+  }
+  console.log('Conectado a base de Datos!');
+});
+
 // Endpoint HTTP
-app.get('/api/data', (req, res) => {
-  // Lógica para obtener datos y devolverlos como respuesta
-  res.send('Aquí están los datos de la API HTTP');
+app.get('/data', (req, res) => {
+  connection.query('SELECT * FROM Votos_data', (error, results, fields) => {
+    if (error) {
+      console.error('Error al hacer la peticion: ', error);
+      res.status(500).send('Error');
+      return;
+    }
+    res.json(results);
+  });
 });
 
 // Endpoint WebSocket
@@ -33,5 +57,5 @@ wss.on('connection', (ws) => {
 
 // Iniciar el servidor HTTP
 server.listen(8080, () => {
-  console.log('Servidor iniciado en el puerto 3000');
+  console.log('Servidor iniciado en el puerto 8080');
 });
