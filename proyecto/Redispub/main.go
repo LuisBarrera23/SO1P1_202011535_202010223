e14ac75819a7a602d5205470db264a272537abca
LuisBarrera23 <luisbarrera5662@gmail.com>
2023-04-19 00:12:36 -0600

    redispub

---
+++ proyecto/Redispub/main.go
@@ -0,0 +1,131 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"strconv"
+
+	"github.com/google/uuid"
+	"github.com/redis/go-redis/v9"
+)
+
+type Data struct {
+	Sede         int    `json:"sede"`
+	Municipio    string `json:"municipio"`
+	Departamento string `json:"departamento"`
+	Papeleta     string `json:"papeleta"`
+	Partido      string `json:"partido"`
+}
+
+func main() {
+	rdb := redis.NewClient(&redis.Options{
+		Addr: "localhost:6379",
+	})
+
+	// Endpoint para almacenar los datos en Redis
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		if r.Method == "POST" {
+			var data Data
+			err := json.NewDecoder(r.Body).Decode(&data)
+			if err != nil {
+				http.Error(w, err.Error(), http.StatusBadRequest)
+				return
+			}
+
+			message, err := json.Marshal(data)
+			if err != nil {
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+				return
+			}
+
+			// Generar una clave UUID única para cada objeto JSON
+			key := uuid.New().String()
+
+			// Almacenar en Redis
+			err = rdb.Set(r.Context(), key, message, 0).Err()
+			if err != nil {
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+				return
+			}
+
+			fmt.Println(string(message))
+
+			// Agregar el JSON a la lista "last_five"
+			err = rdb.LPush(r.Context(), "last_five", string(message)).Err()
+			if err != nil {
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+				return
+			}
+
+			// Mantener solo los últimos 5 elementos en la lista "last_five"
+			err = rdb.LTrim(r.Context(), "last_five", 0, 4).Err()
+			if err != nil {
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+				return
+			}
+
+			// Incrementar el contador de sedes
+			sede := data.Departamento + "_" + data.Municipio + "_" + strconv.Itoa(data.Sede)
+			_, err = rdb.HIncrBy(r.Context(), "sede_counters", sede, 1).Result()
+			if err != nil {
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+				return
+			}
+		} else {
+			http.Error(w, "Invalid request method", http.StatusMethodNotAllowed)
+		}
+	})
+
+	http.HandleFunc("/last_five", func(w http.ResponseWriter, r *http.Request) {
+		if r.Method == "GET" {
+			result, err := rdb.LRange(r.Context(), "last_five", 0, 4).Result()
+			if err != nil {
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+				return
+			}
+
+			w.Header().Set("Content-Type", "application/json")
+			w.WriteHeader(http.StatusOK)
+			json.NewEncoder(w).Encode(result)
+		} else {
+			http.Error(w, "Invalid request method", http.StatusMethodNotAllowed)
+		}
+	})
+
+	http.HandleFunc("/sede_counters", func(w http.ResponseWriter, r *http.Request) {
+		if r.Method == "GET" {
+			// Obtener todos los contadores de sedes
+			counters, err := rdb.HGetAll(r.Context(), "sede_counters").Result()
+			if err != nil {
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+				return
+			}
+
+			// Convertir los valores del hash a un JSON y enviar la respuesta
+			response, err := json.Marshal(counters)
+			if err != nil {
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+				return
+			}
+
+			w.Header().Set("Content-Type", "application/json")
+			w.Write(response)
+		} else {
+			http.Error(w, "Invalid request method", http.StatusMethodNotAllowed)
+		}
+	})
+
+	http.HandleFunc("/delete", func(w http.ResponseWriter, r *http.Request) {
+		_, err := rdb.FlushAll(r.Context()).Result()
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		fmt.Fprintf(w, "All data deleted from Redis")
+	})
+
+	fmt.Println("Listening on :3000")
+	http.ListenAndServe(":3000", nil)
+}
