f7421bf2b270ecf4c7544adeb90a9f22e71cb872
LuisBarrera23 <luisbarrera5662@gmail.com>
2023-04-21 01:08:17 -0600

    redispub y redissub

---
+++ proyecto/Redissub/go.mod
@@ -0,0 +1,11 @@
+module redissub
+
+require (
+	github.com/cespare/xxhash/v2 v2.2.0 // indirect
+	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
+	github.com/go-redis/redis/v8 v8.11.5 // indirect
+	github.com/go-sql-driver/mysql v1.7.0 // indirect
+	github.com/redis/go-redis/v9 v9.0.3 // indirect
+)
+
+go 1.20
---
+++ proyecto/Redissub/main.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"context"
+	"database/sql"
+	"encoding/json"
+	"fmt"
+	"strconv"
+
+	"github.com/go-redis/redis/v8"
+	_ "github.com/go-sql-driver/mysql"
+)
+
+type Data struct {
+	Sede         int    `json:"sede"`
+	Municipio    string `json:"municipio"`
+	Departamento string `json:"departamento"`
+	Papeleta     string `json:"papeleta"`
+	Partido      string `json:"partido"`
+}
+
+func conectarBaseDeDatos() (conexion *sql.DB) {
+	nombreContenedor := "localhost"
+	nombreDB := "db_so1"
+	driver := "mysql"
+	usuario := "root"
+	contrasena := "1234"
+	puerto := "3306"
+
+	conexion, err := sql.Open(driver, usuario+":"+contrasena+"@tcp("+nombreContenedor+":"+puerto+")/"+nombreDB)
+	if err != nil {
+		panic(err.Error())
+	}
+	return conexion
+}
+
+func main() {
+	rdb := redis.NewClient(&redis.Options{
+		Addr: "localhost:6379",
+	})
+
+	ctx := context.Background()
+	pubsub := rdb.Subscribe(ctx, "subir_votos")
+	defer pubsub.Close()
+
+	ch := pubsub.Channel()
+
+	for msg := range ch {
+		fmt.Println(msg.Payload)
+		var data Data
+		err := json.Unmarshal([]byte(msg.Payload), &data)
+		if err != nil {
+			fmt.Printf("Error al decodificar mensaje: %v\n", err)
+			continue
+		}
+		DBconexion := conectarBaseDeDatos()
+
+		datalogs, err := DBconexion.Prepare("INSERT INTO Votos_data (sede, municipio, departamento, papeleta, partido) VALUES('" + strconv.Itoa(data.Sede) + "','" + data.Municipio + "','" + data.Departamento + "','" + data.Papeleta + "','" + data.Partido + "');")
+		if err != nil {
+			panic(err.Error())
+		}
+		datalogs.Exec()
+		DBconexion.Close()
+
+		fmt.Printf("Mensaje recibido: %v\n", data.Departamento)
+	}
+}
