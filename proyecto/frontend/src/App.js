2249348dcea7bace34270cc281d175d521608403
LuisBarrera23 <luisbarrera5662@gmail.com>
2023-04-29 18:02:16 -0600

    frontend modificado

--- proyecto/frontend/src/App.js
+++ proyecto/frontend/src/App.js
@@ -1,5 +1,6 @@
 import Chart from "chart.js/auto";
 import { CategoryScale } from "chart.js";
+import { Table } from "react-bootstrap";
 import React, { useState, useEffect } from "react";
 import GestionPieChart from "./components/GestionPieChart";
 import BarChart from "./components/BarChart";
@@ -11,7 +12,7 @@ import Footer from "./components/footer";
 
 
 Chart.register(CategoryScale);
- 
+
 export default function App() {
   const apiUrl = process.env.REACT_APP_API_HOST;
 
@@ -31,7 +32,7 @@ export default function App() {
         if (redisData.sede_counters.length > 0) {
           setdataBarras(redisData.sede_counters);
         }
-        
+
       }).catch((error) => console.error(error));
     };
     // Se ejecuta fetchData() inmediatamente y luego cada 1 segundo
@@ -39,54 +40,80 @@ export default function App() {
     // Devuelve una función para cancelar el intervalo cuando el componente se desmonte
     return () => clearInterval(intervalId);
   }, []);
-  
 
 
- 
+
+
   return (
     <div className="App">
 
-    <div class="container">
-    <WebSocket/>
-      <div class="row">
-        <div class="col mx-auto">     
+      <div class="container">
+        <WebSocket />
+        <div class="row">
+          <div class="col mx-auto">
 
-        <GestionPieChart data={data}/>
+            <GestionPieChart data={data} />
 
-        <div>
-        
-      </div>
-        </div>
-        <div class="col">
-          <BarChart dataBarras={dataBarras}/> 
-        </div>
-      </div>
-    </div>  
+            <div>
 
-    <div class="container container2">
-      <div class="row">
-        <div class="col mx-auto">        
-        <div>
-        <h3 style={{color:"white"}}>Top 3</h3>
-        <h4>Departamentos con mayores votos para presidente</h4>
-        
-        <Top3/>
-      </div>
-        </div>
-        <div class="col">
-          <h3 style={{color:"white"}}>Ultimos 5 votos</h3>
-          <Tabla dataTabla={dataTabla}></Tabla>
+            </div>
+          </div>
+          <div class="col">
+            <BarChart dataBarras={dataBarras} />
+          </div>
         </div>
       </div>
-    </div> 
-    <div class="container">
-      <h2>Datos almacenados en MySQL</h2>
-      <textarea style={{ height: '400px' }} className="form-control" readOnly value={JSON.stringify(data, null, 2)} />
 
-    </div>
-    
+      <div class="container container2">
+        <div class="row">
+          <div class="col mx-auto">
+            <div>
+              <h3 style={{ color: "white" }}>Top 3</h3>
+              <h4>Departamentos con mayores votos para presidente</h4>
 
-    <Footer></Footer>
+              <Top3 />
+            </div>
+          </div>
+          <div class="col">
+            <h3 style={{ color: "white" }}>Ultimos 5 votos</h3>
+            <Tabla dataTabla={dataTabla}></Tabla>
+          </div>
+        </div>
+      </div>
+      <div class="container">
+        <h2>Datos almacenados en MySQL</h2>
+        <div style={{ height: "400px", overflowY: "scroll" }}>
+          <Table striped bordered hover>
+            <thead className="table-dark" style={{ position: "sticky", top: 0 }}>
+              <tr>
+                <th>No.</th>
+                <th>Sede</th>
+                <th>Municipio</th>
+                <th>Departamento</th>
+                <th>Papeleta</th>
+                <th>Partido</th>
+              </tr>
+            </thead>
+            <tbody>
+              {data.map((dato, index) => (
+                <tr key={index}>
+                  <td>{index + 1}</td>
+                  <td>{dato.sede}</td>
+                  <td>{dato.municipio}</td>
+                  <td>{dato.departamento}</td>
+                  <td>{dato.papeleta}</td>
+                  <td>{dato.partido}</td>
+                </tr>
+              ))}
+            </tbody>
+          </Table>
+        </div>
+
+
+      </div>
+
+
+      <Footer></Footer>
 
     </div>
   );
